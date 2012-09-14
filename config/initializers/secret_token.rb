25d8b200b6c72333654a857bf89faa654321ee45
Derrick Lannaman <derricklannaman@gmail.com>
2012-09-14 10:31:00 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeGps::Application.config.secret_token = '870389ad77c47c90574b7c87802a7ae5f92167dcd1a55584b157dc1e48f8082537f9d8b95ba758202c2f73d558160b18e7f015f3d1c743d66abefd5d97080ef2'
